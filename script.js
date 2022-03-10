da130e7a143efd0d98ade5d963089f05bb9e55ba
Jesse <jesse.movecar@gmail.com>
2022-03-10 17:41:23 +0100

    100%

---
+++ script.js
@@ -0,0 +1,26 @@
+const counterDisplay = document.querySelector('h3');
+let counter = 0;
+
+const bubbleMaker = () => {
+  const bubble = document.createElement('span');
+  bubble.classList.add('bubble');
+  document.body.appendChild(bubble);
+
+  const size = Math.random() * 200 + 100 + 'px';
+  const plusMinus = Math.random() > 0.5 ? 1 : -1;
+
+  bubble.style.height = size;
+  bubble.style.width = size;
+  bubble.style.top = Math.random() * 100 + 50 + '%';
+  bubble.style.left = Math.random() * 100 + '%';
+  bubble.style.setProperty('--left', Math.random() * 100 * plusMinus + '%');
+  bubble.addEventListener('click', () => {
+    bubble.remove();
+    counterDisplay.textContent = counter++;
+  });
+
+  setTimeout(() => {
+    bubble.remove();
+  }, 8000);
+};
+setInterval(bubbleMaker, 300);
